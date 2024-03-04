e510b32c17473276b29f6ec00389521f5e4d85fb
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-03-04 09:00:11 -0300

    Update QJ.py

--- ProgramacaoDeComputadores/codeforces 01/QJ.py
+++ ProgramacaoDeComputadores/codeforces 01/QJ.py
@@ -6,4 +6,12 @@ c10  = (n - (((100*c100) + c50*50) + c20*20))//10
 c5   = (n - ((((100*c100) + c50*50) + c20*20) + c10*10))//5
 c2   = (n - (((((100*c100) + c50*50) + c20*20) + c10*10) + c5*5))//2
 c1   = (n - ((((((100*c100) + c50*50) + c20*20) + c10*10) + c5*5) + c2*2))//1
-print(c100, c50, c20, c10, c5, c2, c1)
+
+print(n)
+print(c100, "nota(s) de R$ 100,00")
+print(c50, "nota(s) de R$ 50,00")
+print(c20, "nota(s) de R$ 20,00")
+print(c10, "nota(s) de R$ 10,00")
+print(c5, "nota(s) de R$ 5,00")
+print(c2, "nota(s) de R$ 2,00")
+print(c1, "nota(s) de R$ 1,00")
