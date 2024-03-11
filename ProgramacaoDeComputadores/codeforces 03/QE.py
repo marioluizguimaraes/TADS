c5d17d0e5471c3615c93eac1abfd5f6db7068470
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-03-11 08:43:09 -0300

    Update QE.py

--- ProgramacaoDeComputadores/codeforces 03/QE.py
+++ ProgramacaoDeComputadores/codeforces 03/QE.py
@@ -1,9 +1,10 @@
 n = int(input())
 resultado = (n + 1)%2
 proximopar = 0
+
 if ( resultado == 0):
     proximopar = n + 1
-    print(proximopar)
 else: 
     proximopar = n + 2
-    print(proximopar)
+    
+print(proximopar)
