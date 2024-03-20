7af4d672fd6e270375a9fa90a530d8bb6815bde6
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-03-20 09:54:42 -0300

    .

--- ProgramacaoDeComputadores/codeforces 04/QI.py
+++ ProgramacaoDeComputadores/codeforces 04/QI.py
@@ -1,16 +1,17 @@
 a, b, c = map(int, input().split())
 
 if (a < b + c and  b < a + c and c < a + b ):
+    
     if (a == b and a == c and b == c): 
        print("a")  
+   
     else:
-        
-        if( b > a and b > c):
+        if(b >= a and b >= c):
             if ( pow(b,2) == pow(a,2) + pow(c,2)):
-              print("r") 
+                print("r") 
             else:
                 print("o")                    
-        elif (c > a and c > b):      
+        elif (c >= a and c >= b):      
             if ( pow(c,2) == pow(a,2) + pow(b,2)):
                 print("r") 
             else:
@@ -20,6 +21,7 @@ if (a < b + c and  b < a + c and c < a + b ):
                 print("r") 
             else:
                 print("o")
+               
 else:
     print("n")
     
