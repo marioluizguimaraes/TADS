8adb3360987298ecbf8806949bc58850d85cee59
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-03-22 07:43:03 -0300

    .

--- ProgramacaoDeComputadores/codeforces 05/QA.py
+++ ProgramacaoDeComputadores/codeforces 05/QA.py
@@ -1,10 +1,8 @@
 n1, d1, v1 = map(int, input().split())
 n2, d2, v2 = map(int, input().split())
 
-vmd1 = d1/v1
-vmd2 = d2/v2
-
-if (d1 > d2 and vmd2 >= vmd1 ):
+if (d1 <= d2 and d2/v2 <= d1/v1 ):
     print(n2)
-else:
+
+elif (d1 >= d2 and d2/v2 >= d1/v1 ):
     print(n1)
