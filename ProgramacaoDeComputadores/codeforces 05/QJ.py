83608facb7aef68167e15038057dd9de94c50832
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-03-26 09:13:33 -0300

    .

--- ProgramacaoDeComputadores/codeforces 05/QJ.py
+++ ProgramacaoDeComputadores/codeforces 05/QJ.py
@@ -1,24 +1,13 @@
 z1, z2, z3, z4 = map(int, input().split())
 
-if (z1 >= z2 and z1 >= z3 and z1 >= z4):
-    if(z1 >= z2 + z3 + z4):
-        print("S")
-    else:
-        print("N")
-elif (z2 >= z1 and z2 >= z3 and z2 >= z4):
-    if (z2 >= z1 + z3 + z4):
-        print("S")
-    else:
-        print("N")
+z1z2 = z1*z2
+z1z3 = z1*z3
+z1z4 = z1*z4
+z2z3 = z2*z3
+z2z4 = z2*z4
+z3z4 = z3*z4
 
-elif (z3 >= z1 and z3 >= z2 and z3 >= z4):
-    if (z3 >= z1 + z2 + z4):
-        print("S")
-    else:
-        print("N")
-
-elif (z4 >= z1 and z4 >= z2 and z4 >= z3):
-    if (z4 >= z1 + z2 + z3):
-        print("S")
-    else:
-        print("N")
+if (z1z2 == z3z4 or z1z3 == z2z4 or z1z4 == z2z3 ):
+    print("S")
+else:
+    print("N")
