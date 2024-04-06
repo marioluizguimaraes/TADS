e8866ecb778561db27b660dac1bcc71b17dbf02e
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-04-06 08:00:51 -0300

    .

--- ProgramacaoDeComputadores/Exercício - Listas/q11.py
+++ ProgramacaoDeComputadores/Exercício - Listas/q11.py
@@ -2,9 +2,11 @@ lista = list(map(int, input().split()))
 tamanho = len(lista)
 
 if ( tamanho > 1 and tamanho%2 == 0):
-    media = sum(lista)//tamanho
+    media = sum(lista)/tamanho
     print(media)
 
 elif(tamanho > 1 and tamanho%2 != 0):
     mediana = tamanho//2
     print(lista[mediana])
+else:
+    print(lista)
---
+++ ProgramacaoDeComputadores/Exercício - Listas/q12.py
@@ -0,0 +1,3 @@
+string = list(map(str, input().split()))
+tamaho = len(string)
+print(tamaho)
