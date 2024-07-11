2ecbafe94701b93b99019b0048ffbb78261455bc
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-07-11 10:23:08 -0300

    .

--- ProgramacaoDeComputadores/Exercício -Funções Recursivas/01.py
+++ ProgramacaoDeComputadores/Exercício -Funções Recursivas/01.py
@@ -1,13 +1,14 @@
-def conta_divisores_r(n, div):
-    if div == 1:
+def conta_divisores_r(n, d):
+    if (d == 1):
         return 1
     else:
-        if n % div == 0:
-            return 1 + conta_divisores_r(n, div - 1)
+        if (n % d == 0):
+            return conta_divisores_r(n, d - 1) + 1
         else:
-            return 0 + conta_divisores_r(n, div - 1)
+            return conta_divisores_r(n, d - 1) + 0
 
 def conta_divisores(n):
     return conta_divisores_r(n, n)
 
+print(conta_divisores(3))
 
