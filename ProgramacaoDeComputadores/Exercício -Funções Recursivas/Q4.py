35d81bc6ebf934e1bc3dfc1640064149d66eef49
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-07-18 08:55:01 -0300

    .

--- ProgramacaoDeComputadores/Exercício -Funções Recursivas/Q4.py
+++ ProgramacaoDeComputadores/Exercício -Funções Recursivas/Q4.py
@@ -1,5 +1,7 @@
 def conta_algarismos(n):
-    
-    if (n/10 > 0):
-        return  
+    if (n//10 == False):
+        return 1
     else:
+        return conta_algarismos(n//10) + 1
+
+print(conta_algarismos(1234))
---
+++ ProgramacaoDeComputadores/Exercício -Funções Recursivas/Q5.py
@@ -0,0 +1,4 @@
+def conta_bits(n):
+    
+
+
