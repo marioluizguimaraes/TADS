5317a7f7dc0b4221815be44a14486ccb32b526cd
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-08-26 20:27:03 -0300

    .

--- ProgramacaoDeComputadores/Exercício -Funções Recursivas/Q4.py
+++ ProgramacaoDeComputadores/Exercício -Funções Recursivas/Q4.py
@@ -1,7 +1,12 @@
+# Escreva uma função recursiva que determine quantos algarismos 
+# são necessários para representar um determinado número, 
+# ignorando os zeros à esquerda. 
+
 def conta_algarismos(n):
     if (n//10 == False):
         return 1
     else:
         return conta_algarismos(n//10) + 1
 
-print(conta_algarismos(1234))
+numero = int(input())
+print(conta_algarismos(numero))
